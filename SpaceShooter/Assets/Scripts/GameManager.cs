0107158c00472e8f3be1d29c8963f8ef11b36c79
pedreror1 <jorge@radian.mx>
2019-08-25 20:21:23 +0900

    Added Main Menu and Ingame Timer

--- SpaceShooter/Assets/Scripts/GameManager.cs
+++ SpaceShooter/Assets/Scripts/GameManager.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject GameUI, MainMenuUI;
     int Score = 9999;
     int Coins = 0;
     int enemiesKilled = 13;
     int TimeRemaining= 60;
     int lifes = 3;
     public static GameManager instance;
-    [SerializeField] Text coinText;
+    [SerializeField] Text coinText, timeText;
+    WaitForSeconds timerCycleLapse = new WaitForSeconds(1f);
+   public enum GameState
+    {
+        MainMenu=0,
+        Game=1,
+        Shop=2,
+        HighScores=3
+    }
+    public GameState currentState = GameState.MainMenu;
     public void AddCoin(int numCoins=1)
     {
         Coins+= numCoins;
@@ -20,8 +31,29 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         instance = this;
-    }
+     }
+    IEnumerator GameTime()
+    {
+        while (TimeRemaining>0)
+        {
+            yield return timerCycleLapse;
+            TimeRemaining--;
+            timeText.text = TimeRemaining.ToString().PadLeft(2, '0');
 
+        }
+    }
+    public void changeState(int newState)
+    {
+        currentState = (GameState)newState;
+        switch(currentState)
+        {
+            case GameState.Game:
+                StartCoroutine(GameTime());
+                MainMenuUI.SetActive(false);
+                GameUI.SetActive(true);
+                break;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
